ae4010cee153c34201534a8565871209eab97ec9
MartixCoder <martixcoder64@gmail.com>
2023-12-22 23:38:26 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import styles from "./App.module.css";
 import Navbar from "./components/Navbar/Navbar";
-import Hero from "./components/Hero/HEro";
+import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
 import Experience from "./components/Exprience/Exprience";
 import Project from "./components/Project/Project";
